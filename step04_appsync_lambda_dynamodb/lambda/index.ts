673c9f66003510927b21e2369a16a991b3712d81
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-08-06 16:15:18 +0500

    Create API with dynmodb
    npm i @aws-cdk/aws-appsync
    npm i @aws-cdk/aws-lambda
    npm i @aws-cdk/aws-dynamodb
    npm i aws-sdk
    create ./schema/schema.gql
    create ./lambda/index.ts
    update ./lib/step04_appsync_lambda_dynamodb-stack.ts

---
+++ step04_appsync_lambda_dynamodb/lambda/index.ts
@@ -0,0 +1,34 @@
+import { DynamoDB } from "aws-sdk";
+const documentClient = new DynamoDB.DocumentClient();
+
+type AppSyncEvent = {
+  info: {
+    fieldName: String;
+  };
+  arguments: {
+    product: Product;
+  };
+};
+
+type Product = {
+  id: String;
+  name: String;
+  price: Number;
+};
+
+exports.handler = async (event: AppSyncEvent) => {
+  if (event.info.fieldName == "welcome") {
+    return "Hello World";
+  } else if (event.info.fieldName == "addProduct") {
+    event.arguments.product.id = "Key" + Math.random();
+    const params = {
+      TableName: process.env.TABLE_NAME || "",
+      Item: event.arguments.product,
+    };
+    const data = await documentClient.put(params).promise();
+    console.log("After Adding ", data);
+    return event.arguments.product;
+  } else {
+    return "Not Found";
+  }
+};
--- step04_appsync_lambda_dynamodb/lib/step04_appsync_lambda_dynamodb-stack.ts
+++ step04_appsync_lambda_dynamodb/lib/step04_appsync_lambda_dynamodb-stack.ts
@@ -1,9 +1,61 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as lambda from "@aws-cdk/aws-lambda";
+import {
+  Duration,
+  Expiration,
+} from "@aws-cdk/aws-appsync/node_modules/@aws-cdk/core";
+import * as dynamo from "@aws-cdk/aws-dynamodb";
 
 export class Step04AppsyncLambdaDynamodbStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // Creates the AppSync API
+    const api = new appsync.GraphqlApi(this, "MyDBApi", {
+      name: "cdk-appsync-dynamo-api",
+      schema: appsync.Schema.fromAsset("schema/schema.gql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: Expiration.after(Duration.days(200)),
+          },
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    const lambda_function = new lambda.Function(this, "DynamoDbLambda", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "index.handler",
+    });
+
+    const lambda_dataSource = api.addLambdaDataSource(
+      "lambdaDatasource",
+      lambda_function
+    );
+
+    lambda_dataSource.createResolver({
+      typeName: "Query",
+      fieldName: "welcome",
+    });
+    lambda_dataSource.createResolver({
+      typeName: "Mutation",
+      fieldName: "addProduct",
+    });
+
+    const productTable = new dynamo.Table(this, "ProductTable", {
+      tableName: "Products",
+      partitionKey: {
+        name: "id",
+        type: dynamo.AttributeType.STRING,
+      },
+    });
+    productTable.grantFullAccess(lambda_function);
+
+    lambda_function.addEnvironment("TABLE_NAME", productTable.tableName);
   }
 }
